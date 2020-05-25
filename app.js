2269dbe033063a17f1d2b01bc594324cd74ba72b
Gabriel <gabrielhvmifnmg@gmail.com>
2020-05-25 10:39:39 -0300

    Adiciona roteamento da página principal com parâmetros
    Troca res.sendfile por res.redirect, possibilitando a passagem de parâmetros.

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ var DetectRTC = require('detectrtc');
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/html/preConsulta.html'));
+    // res.sendFile(path.join(__dirname + '/public/html/preConsulta.html'));
+    res.redirect('/public/html/preConsulta.html?token=8nb1nsi1n1k88asncn7aoma87ba '); //Passing GET values
 });
+
 
 
 // Start the server
